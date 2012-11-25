ab157eccd7e67a29cfe2705a286cb998c010e22f
Henrik Pettersson <h.pettersson@gmail.com>
2012-11-25 23:40:45 +0000

    Added parse function

--- app/scripts/models/track.js
+++ app/scripts/models/track.js
@@ -4,7 +4,7 @@ whitburn.Models.Track = Backbone.Model.extend({
     isInitialized: false
   },
 
-  mode: ['major', 'minor'],
+  modes: ['Major', 'Minor'],
   chromaticScale: ['C', 'D&#9837;', 'D', 'E&#9837;', 'E', 'F', 'G&#9837;', 'G', 'A&#9837;', 'A', 'B&#9837;', 'B'],
 
   initialize: function() {
@@ -16,6 +16,51 @@ whitburn.Models.Track = Backbone.Model.extend({
     } else {
       self.set('isInitialized', true);
     }
+  },
+
+  parse: function(response) {
+    var value,
+        summary = response.audio_summary;
+    if (summary) {
+      value = summary.mode;
+      summary.mode = { value: value, label: this.modes[value] };
+      value = summary.key;
+      summary.key = { value: value, label: this.chromaticScale[value] };
+      summary.tempo = Math.round(parseFloat(summary.tempo));
+
+      summary.energy = summary.energy.toFixed(2);
+      summary.liveness = summary.liveness.toFixed(2);
+      summary.danceability = summary.danceability.toFixed(2);
+    }
+
+    if (response.tracks) {
+      _.each(response.tracks, function(track) {
+        if (track.catalog === "spotify-WW") {
+          response.spotify_link = 'http://open.spotify.com/track/' + track.id;
+        }
+      });
+    }
+
+    var date;
+    if (response.datePeaked) {
+      date = response.dateEntered.replace('+','').split('/');
+      response.date_peaked = new Date(date[2], date[1], date[0]);
+      delete response.datePeaked;
+    }
+    if (response.dateEntered) {
+      date = response.dateEntered.replace('+','').split('/');
+      response.date_entered = new Date(date[2], date[1], date[0]);
+      delete response.dateEntered;
+    } else {
+      response.date_entered = new Date(response.year, 0, 0);
+    }
+    response.no_of_weeks_charted = response.ch;
+    response.yearly_rank = parseInt(response.yearlyRank, 10);
+
+    delete response.ch;
+    delete response.yearlyRank;
+
+    return response;
   },
 
   fetchMetaData: function() {
