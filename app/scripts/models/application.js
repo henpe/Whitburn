0f1a3a828e84838be580d48669f0f433c8100fa7
Henrik Pettersson <h.pettersson@gmail.com>
2012-12-01 23:12:01 +0000

    Added defaults

--- app/scripts/models/application.js
+++ app/scripts/models/application.js
@@ -1,5 +1,12 @@
 whitburn.Models.Application = Backbone.Model.extend({
 
+  defaults: {
+    param_x: 'date',
+    param_y: 'no_of_weeks_charted',
+    param_color: 'song_hotttnesss',
+    param_size: 'duration'
+  },
+
   fetchTracks: function() {
     this.set('tracks', new app.Collections.Tracks());
     return this.get('tracks').fetch();
